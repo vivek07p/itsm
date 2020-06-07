23ab380e32dfed79b65a07dd9129d190696fcab5
VIVEK PANDEY <vivekpandey@VIVEKs-MacBook-Air.local>
2020-06-07 15:04:57 +0530

    initial commit

---
+++ src/main/java/com/service/itsm/ItsmApplication.java
@@ -0,0 +1,13 @@
+package com.service.itsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/itsm/ItsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.itsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
