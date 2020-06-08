f776cb94a1e2907a74615e46134c9c1cfcf5031b
VIVEK PANDEY <vivekpandey@VIVEKs-MacBook-Air.local>
2020-06-08 14:41:28 +0530

    remove comments

--- src/main/java/com/service/itsm/Configuration/WebConfigurer.java
+++ src/main/java/com/service/itsm/Configuration/WebConfigurer.java
@@ -2,29 +2,10 @@ package com.service.itsm.Configuration;
 
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Configuration of web application with Servlet 3.0 APIs.
- */
 @Configuration
 public class WebConfigurer
 //        implements ServletContextInitializer
 {
-
-    /*private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
-
-    private final Environment env;
-
-    public WebConfigurer(Environment env) {
-        this.env = env;
-    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        if (env.getActiveProfiles().length != 0) {
-            log.info("Web application configuration, using profiles: {}", (Object[]) env.getActiveProfiles());
-        }
-        log.info("Web application fully configured");
-    }*/
 
 
 }
