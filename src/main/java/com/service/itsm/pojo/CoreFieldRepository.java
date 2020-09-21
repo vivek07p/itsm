396378de2a9347df9cc1a6dcafa5f408caa91fab
VIVEK PANDEY <vvekpandey@gmail.com>
2020-09-21 21:35:50 +0530

    remove comilation error

--- src/main/java/com/service/itsm/pojo/CoreFieldRepository.java
+++ src/main/java/com/service/itsm/pojo/CoreFieldRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CoreFieldRepository extends JpaRepository<CoreFieldRepository, UUID> {
+public interface CoreFieldRepository extends JpaRepository<CoreField, UUID> {
 }
